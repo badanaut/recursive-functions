4d4adbdbec373ac7391534c550eed1f032a9c644
badanaut <49814350+badanaut@users.noreply.github.com>
2023-04-01 23:10:12 +0300

    Add sortMerge function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 function fibs(n) {
   const fib = [0, 1];
   if (n <= 1) {
@@ -18,3 +19,27 @@ function fibsRec(num, arr = [0, 1]) {
   return arr.length >= num ? arr : fibsRec(num, [...arr, arr[arr.length - 2] + arr[arr.length - 1]]);
 }
 console.log(fibsRec(9));
+
+function mergeSort(array) {
+  if (array.length === 1) return array;
+  const sortedArray = [];
+  const middle = Math.trunc(array.length / 2);
+  const leftSide = mergeSort(array.slice(0, middle));
+  const rightSide = mergeSort(array.slice(middle));
+
+  ((left, right) => {
+    let i = 0;
+    let j = 0;
+
+    while (i < left.length && j < right.length) {
+      // eslint-disable-next-line no-unused-expressions
+      left[i] < right[j] ? sortedArray.push(left[i++]) : sortedArray.push(right[j++]);
+    }
+    while (i < left.length) sortedArray.push(left[i++]);
+    while (j < right.length) sortedArray.push(right[j++]);
+  })(leftSide, rightSide);
+
+  return sortedArray;
+}
+
+console.log(mergeSort([4, 2, 7, 1, 9, 5]));
